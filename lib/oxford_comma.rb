2ed3186c59db773d067c6b3c0d9c7d7e769a1a29
--replace-all <terrencejung928@gmail.com>
2022-06-28 18:50:46 -0700

    commit tree ba4aef125fab2ec718a923df94b1c8654b3f2c26

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+#Given an array of strings, format it into a string 
 def oxford_comma(array)
+    if array.length < 3
+        array.join(" and ")
+    elsif array.length >= 3
+        array[-1] = "and #{array[-1]}"
 
+        array.join(", ")
+    end
 end
